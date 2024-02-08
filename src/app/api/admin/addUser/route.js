295ff0c1fd5fd63029e1656cdca02caa930c3067
AleksNankiewicz <151682321+AleksNankiewicz@users.noreply.github.com>
2024-02-08 21:25:54 +0100

    fixed stuff4

--- src/app/api/admin/addUser/route.js
+++ src/app/api/admin/addUser/route.js
@@ -1,15 +1,15 @@
-import { User } from '@/lib/models'
-import { connectToDb } from '@/lib/utils'
-export async function POST(request) {
-  const { username, fullName, password } = await request.json()
+// import { User } from '@/lib/models'
+// import { connectToDb } from '@/lib/utils'
+// export async function POST(request) {
+//   const { username, fullName, password } = await request.json()
 
-  try {
-    await connectToDb()
-    const user = new User({ username, fullName, password })
+//   try {
+//     await connectToDb()
+//     const user = new User({ username, fullName, password })
 
-    await user.save()
-    console.log('saved to db')
-  } catch (err) {
-    console.log(err)
-  }
-}
+//     await user.save()
+//     console.log('saved to db')
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
--- src/app/api/userByEmail/route.js
+++ src/app/api/userByEmail/route.js
@@ -1,3 +1,3 @@
-export async function GET(request) {
-  console.log(request)
-}
+// export async function GET(request) {
+//   console.log(request)
+// }
